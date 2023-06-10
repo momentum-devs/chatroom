849a3a26eb60d2ba297cc4c0c82835e809f0fdf2
Wojciech Sekta <sekta.wojciech@gmail.com>
2023-06-11 00:22:34 +0200

    add ninja

--- externals/install_externals.sh
+++ externals/install_externals.sh
@@ -4,8 +4,8 @@ cd googletest || exit
 rm build
 mkdir build
 cd build || exit
-cmake .. -DCMAKE_BUILD_TYPE:STRING=$BUILD_TYPE -DCMAKE_CXX_COMPILER=/usr/bin/clang++-15
-make -j 20
+cmake .. -DCMAKE_BUILD_TYPE:STRING=$BUILD_TYPE -DCMAKE_CXX_COMPILER=/usr/bin/clang++-15 -GNinja
+ninja -j 20
 sudo make install
 cd ../..
 
@@ -13,8 +13,8 @@ cd json || exit
 rm build
 mkdir build
 cd build || exit
-cmake .. -DCMAKE_BUILD_TYPE:STRING=$BUILD_TYPE -DCMAKE_CXX_COMPILER=/usr/bin/clang++-15
-make -j 20
+cmake .. -DCMAKE_BUILD_TYPE:STRING=$BUILD_TYPE -DCMAKE_CXX_COMPILER=/usr/bin/clang++-15 -GNinja
+ninja -j 20
 sudo make install
 cd ../..
 
@@ -22,8 +22,8 @@ cd tabulate || exit
 rm build
 mkdir build
 cd build || exit
-cmake .. -DUSE_CPP17=ON -DCMAKE_BUILD_TYPE:STRING=$BUILD_TYPE -DCMAKE_CXX_COMPILER=/usr/bin/clang++-15
-make -j 20
+cmake .. -DUSE_CPP17=ON -DCMAKE_BUILD_TYPE:STRING=$BUILD_TYPE -DCMAKE_CXX_COMPILER=/usr/bin/clang++-15 -GNinja
+ninja -j 20
 sudo make install
 cd ../..
 
@@ -32,6 +32,6 @@ sed '146d' -i cmake/CommonModules/TinyCommon.cmake || exit
 rm build
 mkdir build
 cd build || exit
-cmake .. -DTOM=ON -DCMAKE_BUILD_TYPE:STRING=$BUILD_TYPE -DCMAKE_CXX_COMPILER=/usr/bin/clang++-15
-make -j 20
+cmake .. -DTOM=ON -DCMAKE_BUILD_TYPE:STRING=$BUILD_TYPE -DCMAKE_CXX_COMPILER=/usr/bin/clang++-15 -GNinja
+ninja -j 20
 sudo make install
