a4d0e5486f0a3db8eae82d35d6493d078b87810a
Wojciech Sekta <sekta.wojciech@gmail.com>
2023-06-10 22:55:48 +0200

    add Release build option

--- externals/install_externals.sh
+++ externals/install_externals.sh
@@ -1,8 +1,10 @@
+[ -z "$BUILD_TYPE" ] && export BUILD_TYPE='Debug'
+
 cd googletest || exit
 rm build
 mkdir build
 cd build || exit
-cmake .. -DCMAKE_BUILD_TYPE:STRING='Debug' -DCMAKE_CXX_COMPILER=/usr/bin/clang++-15
+cmake .. -DCMAKE_BUILD_TYPE:STRING=$BUILD_TYPE -DCMAKE_CXX_COMPILER=/usr/bin/clang++-15
 make -j 20
 sudo make install
 cd ../..
@@ -11,7 +13,7 @@ cd json || exit
 rm build
 mkdir build
 cd build || exit
-cmake .. -DCMAKE_BUILD_TYPE:STRING='Debug' -DCMAKE_CXX_COMPILER=/usr/bin/clang++-15
+cmake .. -DCMAKE_BUILD_TYPE:STRING=$BUILD_TYPE -DCMAKE_CXX_COMPILER=/usr/bin/clang++-15
 make -j 20
 sudo make install
 cd ../..
@@ -20,7 +22,7 @@ cd tabulate || exit
 rm build
 mkdir build
 cd build || exit
-cmake .. -DUSE_CPP17=ON -DCMAKE_BUILD_TYPE:STRING='Debug' -DCMAKE_CXX_COMPILER=/usr/bin/clang++-15
+cmake .. -DUSE_CPP17=ON -DCMAKE_BUILD_TYPE:STRING=$BUILD_TYPE -DCMAKE_CXX_COMPILER=/usr/bin/clang++-15
 make -j 20
 sudo make install
 cd ../..
@@ -30,6 +32,6 @@ sed '146d' -i cmake/CommonModules/TinyCommon.cmake || exit
 rm build
 mkdir build
 cd build || exit
-cmake .. -DTOM=ON -DCMAKE_BUILD_TYPE:STRING='Debug' -DCMAKE_CXX_COMPILER=/usr/bin/clang++-15
+cmake .. -DTOM=ON -DCMAKE_BUILD_TYPE:STRING=$BUILD_TYPE -DCMAKE_CXX_COMPILER=/usr/bin/clang++-15
 make -j 20
 sudo make install
