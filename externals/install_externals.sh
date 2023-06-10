fcb173773f71d535274cb0d264d3a769c9e2c6af
Wojciech Sekta <sekta.wojciech@gmail.com>
2023-06-11 01:39:15 +0200

    upgrade clang to version 16

--- externals/install_externals.sh
+++ externals/install_externals.sh
@@ -4,7 +4,7 @@ cd googletest || exit
 rm build
 mkdir build
 cd build || exit
-cmake .. -DCMAKE_BUILD_TYPE:STRING=$BUILD_TYPE -DCMAKE_CXX_COMPILER=/usr/bin/clang++-15 -GNinja
+cmake .. -DCMAKE_BUILD_TYPE:STRING=$BUILD_TYPE -DCMAKE_CXX_COMPILER=/usr/bin/clang++-16 -GNinja
 ninja -j 20
 sudo ninja install
 cd ../..
@@ -13,7 +13,7 @@ cd json || exit
 rm build
 mkdir build
 cd build || exit
-cmake .. -DCMAKE_BUILD_TYPE:STRING=$BUILD_TYPE -DCMAKE_CXX_COMPILER=/usr/bin/clang++-15 -GNinja
+cmake .. -DCMAKE_BUILD_TYPE:STRING=$BUILD_TYPE -DCMAKE_CXX_COMPILER=/usr/bin/clang++-16 -GNinja
 ninja -j 20
 sudo ninja install
 cd ../..
@@ -22,7 +22,7 @@ cd tabulate || exit
 rm build
 mkdir build
 cd build || exit
-cmake .. -DUSE_CPP17=ON -DCMAKE_BUILD_TYPE:STRING=$BUILD_TYPE -DCMAKE_CXX_COMPILER=/usr/bin/clang++-15 -GNinja
+cmake .. -DUSE_CPP17=ON -DCMAKE_BUILD_TYPE:STRING=$BUILD_TYPE -DCMAKE_CXX_COMPILER=/usr/bin/clang++-16 -GNinja
 ninja -j 20
 sudo ninja install
 cd ../..
@@ -32,6 +32,6 @@ sed '146d' -i cmake/CommonModules/TinyCommon.cmake || exit
 rm build
 mkdir build
 cd build || exit
-cmake .. -DTOM=ON -DCMAKE_BUILD_TYPE:STRING=$BUILD_TYPE -DCMAKE_CXX_COMPILER=/usr/bin/clang++-15 -GNinja
+cmake .. -DTOM=ON -DCMAKE_BUILD_TYPE:STRING=$BUILD_TYPE -DCMAKE_CXX_COMPILER=/usr/bin/clang++-16 -GNinja
 ninja -j 20
 sudo ninja install
