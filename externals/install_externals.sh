08c799f9a96d37f44f069546e6886abe9eee97f4
Wojciech Sekta <sekta.wojciech@gmail.com>
2023-06-11 09:14:34 +0200

    install cpp-jwt externally

--- externals/install_externals.sh
+++ externals/install_externals.sh
@@ -1,37 +1,48 @@
 [ -z "$BUILD_TYPE" ] && export BUILD_TYPE='Debug'
 
+COMMON_CMAKE_FLAGS='-DCMAKE_BUILD_TYPE:STRING=$BUILD_TYPE -DCMAKE_CXX_COMPILER=/usr/bin/clang++-16 -GNinja'
+
 cd googletest || exit
-rm build
+sudo rm -rf build
 mkdir build
 cd build || exit
-cmake .. -DCMAKE_BUILD_TYPE:STRING=$BUILD_TYPE -DCMAKE_CXX_COMPILER=/usr/bin/clang++-16 -GNinja
+eval cmake .. $COMMON_CMAKE_FLAGS
 ninja
 sudo ninja install
 cd ../..
 
 cd json || exit
-rm build
+sudo rm -rf build
 mkdir build
 cd build || exit
-cmake .. -DCMAKE_BUILD_TYPE:STRING=$BUILD_TYPE -DCMAKE_CXX_COMPILER=/usr/bin/clang++-16 -GNinja
+eval cmake .. -DJSON_BuildTests=OFF $COMMON_CMAKE_FLAGS
 ninja
 sudo ninja install
 cd ../..
 
 cd tabulate || exit
-rm build
+sudo rm -rf build
 mkdir build
 cd build || exit
-cmake .. -DUSE_CPP17=ON -DCMAKE_BUILD_TYPE:STRING=$BUILD_TYPE -DCMAKE_CXX_COMPILER=/usr/bin/clang++-16 -GNinja
+eval cmake .. -DUSE_CPP17=ON $COMMON_CMAKE_FLAGS
 ninja
 sudo ninja install
 cd ../..
 
 cd TinyORM || exit
-sed '146d' -i cmake/CommonModules/TinyCommon.cmake || exit
-rm build
+sudo rm -rf build
 mkdir build
 cd build || exit
-cmake .. -DTOM=ON -DCMAKE_BUILD_TYPE:STRING=$BUILD_TYPE -DCMAKE_CXX_COMPILER=/usr/bin/clang++-16 -GNinja
+eval cmake .. -DTOM=ON $COMMON_CMAKE_FLAGS
 ninja
 sudo ninja install
+cd ../..
+
+cd cpp-jwt || exit
+sudo rm -rf build
+mkdir build
+cd build || exit
+eval cmake ..  -DCPP_JWT_BUILD_EXAMPLES=OFF -DCPP_JWT_BUILD_TESTS=OFF $COMMON_CMAKE_FLAGS
+ninja
+sudo ninja install
+cd ../..
