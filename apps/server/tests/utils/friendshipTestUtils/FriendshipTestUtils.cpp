3fe7a006f8e894ae58ab732006c5183caca4682c
Michal Cieslar <michalovskyyy@gmail.com>
2023-10-04 22:53:01 +0200

    add friendship test utils

---
+++ apps/server/tests/utils/friendshipTestUtils/FriendshipTestUtils.cpp
@@ -0,0 +1,65 @@
+#include "FriendshipTestUtils.h"
+
+#include <format>
+#include <utility>
+
+#include "Friendship.odb.h"
+#include "server/infrastructure/database/tables/FriendshipTable.h"
+
+namespace server::tests
+{
+
+FriendshipTestUtils::FriendshipTestUtils(std::shared_ptr<odb::pgsql::database> databaseClientInit)
+    : databaseClient{std::move(databaseClientInit)},
+      friendshipTestFactory{std::make_unique<FriendshipTestFactory>()},
+      userTestFactory{std::make_unique<UserTestFactory>()}
+{
+}
+
+void FriendshipTestUtils::persist(const std::shared_ptr<infrastructure::Friendship>& friendship)
+{
+    odb::transaction transaction(databaseClient->begin());
+
+    databaseClient->persist(friendship);
+
+    transaction.commit();
+}
+
+std::shared_ptr<infrastructure::Friendship>
+FriendshipTestUtils::createAndPersist(const std::shared_ptr<infrastructure::User>& userInput,
+                                      const std::shared_ptr<infrastructure::User>& userFriendInput)
+{
+    const auto user = userInput ? userInput : userTestFactory->createPersistentUser();
+
+    const auto userFriend = userFriendInput ? userFriendInput : userTestFactory->createPersistentUser();
+
+    auto friendship = friendshipTestFactory->createPersistentFriendship(user, userFriend);
+
+    persist(friendship);
+
+    return friendship;
+}
+
+std::shared_ptr<infrastructure::Friendship> FriendshipTestUtils::findById(const std::string& id)
+{
+    typedef odb::query<infrastructure::Friendship> query;
+
+    odb::transaction transaction(databaseClient->begin());
+
+    std::shared_ptr<infrastructure::Friendship> foundFriendship(
+        databaseClient->query_one<infrastructure::Friendship>(query::id == id));
+
+    transaction.commit();
+
+    return foundFriendship;
+}
+
+void FriendshipTestUtils::truncateTable()
+{
+    odb::transaction transaction(databaseClient->begin());
+
+    databaseClient->execute(std::format("DELETE FROM \"{}\";", infrastructure::friendshipTableName));
+
+    transaction.commit();
+}
+}
---
+++ apps/server/tests/utils/friendshipTestUtils/FriendshipTestUtils.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <memory>
+#include <odb/pgsql/database.hxx>
+
+#include "Friendship.h"
+#include "server/tests/factories/friendshipTestFactory/FriendshipTestFactory.h"
+#include "server/tests/factories/userTestFactory/UserTestFactory.h"
+#include "User.h"
+
+namespace server::tests
+{
+class FriendshipTestUtils
+{
+public:
+    explicit FriendshipTestUtils(std::shared_ptr<odb::pgsql::database> databaseClient);
+
+    void persist(const std::shared_ptr<infrastructure::Friendship>& friendship);
+    std::shared_ptr<infrastructure::Friendship>
+    createAndPersist(const std::shared_ptr<infrastructure::User>& user,
+                     const std::shared_ptr<infrastructure::User>& userFriend);
+    std::shared_ptr<infrastructure::Friendship> findById(const std::string& id);
+    void truncateTable();
+
+private:
+    std::shared_ptr<odb::pgsql::database> databaseClient;
+    std::unique_ptr<FriendshipTestFactory> friendshipTestFactory;
+    std::unique_ptr<UserTestFactory> userTestFactory;
+};
+}
