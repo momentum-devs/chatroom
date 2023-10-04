263ede208a32f75e592bc7381d8049fecc6ff0d7
Michal Cieslar <michalovskyyy@gmail.com>
2023-10-04 22:46:51 +0200

    add channel invitation test utils

---
+++ apps/server/tests/utils/channelInvitationTestUtils/ChannelInvitationTestUtils.cpp
@@ -0,0 +1,70 @@
+#include "ChannelInvitationTestUtils.h"
+
+#include <format>
+#include <utility>
+
+#include "ChannelInvitation.odb.h"
+#include "server/infrastructure/database/tables/ChannelInvitationTable.h"
+
+namespace server::tests
+{
+
+ChannelInvitationTestUtils::ChannelInvitationTestUtils(std::shared_ptr<odb::pgsql::database> databaseClientInit)
+    : databaseClient{std::move(databaseClientInit)},
+      channelInvitationTestFactory{std::make_unique<ChannelInvitationTestFactory>()},
+      channelTestFactory{std::make_unique<ChannelTestFactory>()},
+      userTestFactory{std::make_unique<UserTestFactory>()}
+{
+}
+
+void ChannelInvitationTestUtils::persist(const std::shared_ptr<infrastructure::ChannelInvitation>& channelInvitation)
+{
+    odb::transaction transaction(databaseClient->begin());
+
+    databaseClient->persist(channelInvitation);
+
+    transaction.commit();
+}
+
+std::shared_ptr<infrastructure::ChannelInvitation>
+ChannelInvitationTestUtils::createAndPersist(const std::shared_ptr<infrastructure::User>& senderInput,
+                                             const std::shared_ptr<infrastructure::User>& recipientInput,
+                                             const std::shared_ptr<infrastructure::Channel>& channelInput)
+{
+    const auto sender = senderInput ? senderInput : userTestFactory->createPersistentUser();
+
+    const auto recipient = recipientInput ? recipientInput : userTestFactory->createPersistentUser();
+
+    const auto channel = channelInput ? channelInput : channelTestFactory->createPersistentChannel(sender);
+
+    auto channelInvitation =
+        channelInvitationTestFactory->createPersistentChannelInvitation(sender, recipient, channel);
+
+    persist(channelInvitation);
+
+    return channelInvitation;
+}
+
+std::shared_ptr<infrastructure::ChannelInvitation> ChannelInvitationTestUtils::findById(const std::string& id)
+{
+    typedef odb::query<infrastructure::ChannelInvitation> query;
+
+    odb::transaction transaction(databaseClient->begin());
+
+    std::shared_ptr<infrastructure::ChannelInvitation> foundChannelInvitation(
+        databaseClient->query_one<infrastructure::ChannelInvitation>(query::id == id));
+
+    transaction.commit();
+
+    return foundChannelInvitation;
+}
+
+void ChannelInvitationTestUtils::truncateTable()
+{
+    odb::transaction transaction(databaseClient->begin());
+
+    databaseClient->execute(std::format("DELETE FROM \"{}\";", infrastructure::channelInvitationTableName));
+
+    transaction.commit();
+}
+}
---
+++ apps/server/tests/utils/channelInvitationTestUtils/ChannelInvitationTestUtils.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <memory>
+#include <odb/pgsql/database.hxx>
+
+#include "Channel.h"
+#include "server/tests/factories/channelInvitationTestFactory/ChannelInvitationTestFactory.h"
+#include "server/tests/factories/channelTestFactory/ChannelTestFactory.h"
+#include "server/tests/factories/userChannelTestFactory/UserChannelTestFactory.h"
+#include "server/tests/factories/userTestFactory/UserTestFactory.h"
+#include "User.h"
+#include "UserChannel.h"
+
+namespace server::tests
+{
+class ChannelInvitationTestUtils
+{
+public:
+    explicit ChannelInvitationTestUtils(std::shared_ptr<odb::pgsql::database> databaseClient);
+
+    void persist(const std::shared_ptr<infrastructure::ChannelInvitation>& channelInvitation);
+    std::shared_ptr<infrastructure::ChannelInvitation>
+    createAndPersist(const std::shared_ptr<infrastructure::User>& sender,
+                     const std::shared_ptr<infrastructure::User>& recipient,
+                     const std::shared_ptr<infrastructure::Channel>& channel);
+    std::shared_ptr<infrastructure::ChannelInvitation> findById(const std::string& id);
+    void truncateTable();
+
+private:
+    std::shared_ptr<odb::pgsql::database> databaseClient;
+    std::unique_ptr<ChannelInvitationTestFactory> channelInvitationTestFactory;
+    std::unique_ptr<ChannelTestFactory> channelTestFactory;
+    std::unique_ptr<UserTestFactory> userTestFactory;
+};
+}
