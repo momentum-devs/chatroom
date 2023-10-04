2d001ae6f9e23cef8c4739d90a021f324239c651
Michal Cieslar <michalovskyyy@gmail.com>
2023-10-04 20:38:30 +0200

    add friendship test factory

---
+++ apps/server/tests/factories/friendshipTestFactory/FriendshipTestFactory.cpp
@@ -0,0 +1,29 @@
+#include "FriendshipTestFactory.h"
+
+#include "faker-cxx/Date.h"
+#include "faker-cxx/String.h"
+
+namespace server::tests
+{
+std::shared_ptr<domain::Friendship>
+FriendshipTestFactory::createDomainFriendship(const std::shared_ptr<domain::User>& user,
+                                              const std::shared_ptr<domain::User>& userFriend)
+{
+    const auto id = faker::String::uuid();
+    const auto createdAt = faker::Date::pastDate();
+    const auto updatedAt = faker::Date::recentDate();
+
+    return std::make_shared<domain::Friendship>(id, user, userFriend, createdAt, updatedAt);
+}
+
+std::shared_ptr<infrastructure::Friendship>
+FriendshipTestFactory::createPersistentFriendship(const std::shared_ptr<infrastructure::User>& user,
+                                                  const std::shared_ptr<infrastructure::User>& userFriend)
+{
+    const auto id = faker::String::uuid();
+    const auto createdAt = faker::Date::pastDate();
+    const auto updatedAt = faker::Date::recentDate();
+
+    return std::make_shared<infrastructure::Friendship>(id, user, userFriend, createdAt, updatedAt);
+}
+}
---
+++ apps/server/tests/factories/friendshipTestFactory/FriendshipTestFactory.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <memory>
+
+#include "Channel.h"
+#include "Friendship.h"
+#include "server/domain/entities/friendship/Friendship.h"
+#include "server/domain/entities/user/User.h"
+#include "server/domain/entities/userChannel/UserChannel.h"
+#include "User.h"
+
+namespace server::tests
+{
+class FriendshipTestFactory
+{
+public:
+    static std::shared_ptr<domain::Friendship> createDomainFriendship(const std::shared_ptr<domain::User>& user,
+                                                                      const std::shared_ptr<domain::User>& userFriend);
+    static std::shared_ptr<infrastructure::Friendship>
+    createPersistentFriendship(const std::shared_ptr<infrastructure::User>& user,
+                               const std::shared_ptr<infrastructure::User>& userFriend);
+};
+}
