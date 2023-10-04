5fd41dee8b53dba48568aed69afd51a3a19fe35e
Michal Cieslar <michalovskyyy@gmail.com>
2023-10-04 22:57:41 +0200

    add conversation test utils

---
+++ apps/server/tests/utils/conversationTestUtils/ConversationTestUtils.cpp
@@ -0,0 +1,82 @@
+#include "ConversationTestUtils.h"
+
+#include <format>
+#include <utility>
+
+#include "Conversation.odb.h"
+#include "server/infrastructure/database/tables/ChannelTable.h"
+#include "server/infrastructure/database/tables/ConversationTable.h"
+
+namespace server::tests
+{
+
+ConversationTestUtils::ConversationTestUtils(std::shared_ptr<odb::pgsql::database> databaseClientInit)
+    : databaseClient{std::move(databaseClientInit)},
+      conversationTestFactory{std::make_unique<ConversationTestFactory>()},
+      channelTestFactory{std::make_unique<ChannelTestFactory>()},
+      userTestFactory{std::make_unique<UserTestFactory>()}
+{
+}
+
+void ConversationTestUtils::persist(const std::shared_ptr<infrastructure::Conversation>& conversation)
+{
+    odb::transaction transaction(databaseClient->begin());
+
+    databaseClient->persist(conversation);
+
+    transaction.commit();
+}
+
+std::shared_ptr<infrastructure::Conversation>
+ConversationTestUtils::createAndPersist(const std::shared_ptr<infrastructure::User>& userInput,
+                                        const std::shared_ptr<infrastructure::User>& recipientInput)
+{
+    const auto user = userInput ? userInput : userTestFactory->createPersistentUser();
+
+    const auto recipient = recipientInput ? recipientInput : userTestFactory->createPersistentUser();
+
+    auto conversation = conversationTestFactory->createPersistentUsersConversation(user, recipient);
+
+    persist(conversation);
+
+    return conversation;
+}
+
+std::shared_ptr<infrastructure::Conversation>
+ConversationTestUtils::createAndPersist(const std::shared_ptr<infrastructure::Channel>& channelInput)
+{
+    const auto channelCreator = userTestFactory->createPersistentUser();
+
+    const auto channel = channelInput ? channelInput : channelTestFactory->createPersistentChannel(channelCreator);
+
+    auto conversation = conversationTestFactory->createPersistentChannelConversation(channel);
+
+    persist(conversation);
+
+    return conversation;
+}
+
+std::shared_ptr<infrastructure::Conversation> ConversationTestUtils::findById(const std::string& id)
+{
+    typedef odb::query<infrastructure::Conversation> query;
+
+    odb::transaction transaction(databaseClient->begin());
+
+    std::shared_ptr<infrastructure::Conversation> foundConversation(
+        databaseClient->query_one<infrastructure::Conversation>(query::id == id));
+
+    transaction.commit();
+
+    return foundConversation;
+}
+
+void ConversationTestUtils::truncateTable()
+{
+    odb::transaction transaction(databaseClient->begin());
+
+    databaseClient->execute(std::format("DELETE FROM \"{}\";", infrastructure::conversationTableName));
+
+    transaction.commit();
+}
+
+}
---
+++ apps/server/tests/utils/conversationTestUtils/ConversationTestUtils.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <memory>
+#include <odb/pgsql/database.hxx>
+
+#include "Channel.h"
+#include "Conversation.h"
+#include "server/tests/factories/channelTestFactory/ChannelTestFactory.h"
+#include "server/tests/factories/conversationTestFactory/ConversationTestFactory.h"
+#include "server/tests/factories/userTestFactory/UserTestFactory.h"
+#include "User.h"
+
+namespace server::tests
+{
+class ConversationTestUtils
+{
+public:
+    explicit ConversationTestUtils(std::shared_ptr<odb::pgsql::database> databaseClient);
+
+    void persist(const std::shared_ptr<infrastructure::Conversation>& conversation);
+    std::shared_ptr<infrastructure::Conversation>
+    createAndPersist(const std::shared_ptr<infrastructure::User>& user,
+                     const std::shared_ptr<infrastructure::User>& recipient);
+    std::shared_ptr<infrastructure::Conversation>
+    createAndPersist(const std::shared_ptr<infrastructure::Channel>& channel);
+    std::shared_ptr<infrastructure::Conversation> findById(const std::string& id);
+    void truncateTable();
+
+private:
+    std::shared_ptr<odb::pgsql::database> databaseClient;
+    std::unique_ptr<ConversationTestFactory> conversationTestFactory;
+    std::unique_ptr<ChannelTestFactory> channelTestFactory;
+    std::unique_ptr<UserTestFactory> userTestFactory;
+};
+}
