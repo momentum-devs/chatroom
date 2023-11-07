f3278084971df1367ddf5983efb8bbecd8caa561
Michal Cieslar <michalovskyyy@gmail.com>
2023-11-07 19:06:15 +0100

    add example avatar to resources

--- scripts/localstack-setup.sh
+++ scripts/localstack-setup.sh
@@ -2,18 +2,16 @@
 
 echo "CREATING S3 BUCKETS"
 
-awslocal s3 mb s3://videos
-awslocal s3 mb s3://resized-videos
-
-echo "S3 BUCKETS CREATED"
+awslocal s3 mb s3://avatars
 
 awslocal s3 ls
 
+echo "S3 BUCKETS CREATED"
+
 echo "CREATING S3 OBJECTS"
 
-awslocal s3 cp /usr/share/app/resources/sample_file1.mp4 s3://videos/sample_file1.mp4
+awslocal s3 cp /usr/share/app/resources/example_avatar.jpg s3://avatars/example_avatar.jpg
 
-awslocal s3 ls s3://videos --recursive --human-readable --summarize
+awslocal s3 ls s3://avatars --recursive --human-readable --summarize
 
 echo "S3 OBJECTS CREATED"
-
