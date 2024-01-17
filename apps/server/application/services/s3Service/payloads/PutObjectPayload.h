90e3db4430661254d1c7c3eeaae443b1db9361d9
Michal Cieslar <michalovskyyy@gmail.com>
2024-01-17 22:10:53 +0100

    implement uploading object to s3

--- apps/server/application/services/s3Service/payloads/PutObjectPayload.h
+++ apps/server/application/services/s3Service/payloads/PutObjectPayload.h
@@ -7,13 +7,13 @@ namespace server::application
 struct PutObjectPayload
 {
     std::string bucketName;
-    std::string key;
+    std::string objectKey;
     std::string data;
 };
 
 inline bool operator==(const PutObjectPayload& lhs, const PutObjectPayload& rhs)
 {
-    return (lhs.bucketName == rhs.bucketName && lhs.key == rhs.key && lhs.data == rhs.data);
+    return (lhs.bucketName == rhs.bucketName && lhs.objectKey == rhs.objectKey && lhs.data == rhs.data);
 }
 
 }
--- apps/server/infrastructure/services/s3Service/S3ServiceImpl.cpp
+++ apps/server/infrastructure/services/s3Service/S3ServiceImpl.cpp
@@ -1,6 +1,7 @@
 #include "S3ServiceImpl.h"
 
 #include <aws/s3/model/GetObjectRequest.h>
+#include <aws/s3/model/PutObjectRequest.h>
 #include <aws/s3/S3Client.h>
 
 #include "loguru.hpp"
@@ -10,7 +11,28 @@ namespace server::application
 
 S3ServiceImpl::S3ServiceImpl(std::unique_ptr<Aws::S3::S3Client> s3ClientInit) : s3Client{std::move(s3ClientInit)} {}
 
-void S3ServiceImpl::putObject(const PutObjectPayload&) const {}
+void S3ServiceImpl::putObject(const PutObjectPayload& payload) const
+{
+    Aws::S3::Model::PutObjectRequest request;
+
+    request.SetBucket(payload.bucketName);
+    request.SetKey(payload.objectKey);
+
+    request.SetBody(std::make_shared<Aws::StringStream>(payload.data));
+
+    auto result = s3Client->PutObject(request);
+
+    if (result.IsSuccess())
+    {
+        LOG_S(INFO) << "Successfully uploaded to S3.";
+    }
+    else
+    {
+        LOG_S(ERROR) << "Error uploading file to S3: " << result.GetError().GetMessage();
+
+        throw std::runtime_error{result.GetError().GetMessage()};
+    }
+}
 
 std::string S3ServiceImpl::getObject(const GetObjectPayload& payload) const
 {
